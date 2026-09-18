9ba7e8c9e44927154b692d3968d35d3c47575bfe
Sunny Li <sli79@smith.edu>
2026-09-18 13:44:14 -0400

    updated hw01

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Sunny Li
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -45,14 +45,32 @@ def main():
     # Part 1: Basic Operations
     # =============================================
     # Your code for part 1 under this line and before the print statements
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    result1 = (3*x-9*y)/(2*a*(b-c)) # Calculating result 1 using the formula (3x-9y)/(2a(b-c))
 
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
     # End of Part 1 ----------------------
 
 
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
+    x= 5
+    y = -3
+    result2 = (x**2)*(y**4) # Uses ** to perform exponent calculation.
 
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
     # End of Part 2 ----------------------
 
 
@@ -60,14 +78,22 @@ def main():
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
+    a = 100
+    b = 13
+    result3 = a//b # Uses // to perform whole number (integer) calculation.
 
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result3)
     # End of Part 3 ----------------------
 
 
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
+    result4 = a % b # Uses % to perform remainder (leftover) in whole number (integer).
 
+    print("Part 4: result =", result4)
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
